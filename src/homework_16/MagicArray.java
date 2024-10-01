7aae167bd50b2b4e53ebc66a70e9bd101c3a1a6e
s-saratov <saratovs@yandex.ru>
2024-10-01 18:00:06 +0200

    HW 16

--- src/homework_16/MagicArray.java
+++ src/homework_16/MagicArray.java
@@ -4,63 +4,48 @@ public class MagicArray {
     int[] array;
     int cursor; // присвоено значение по умолчанию = 0;
 
-    // Конструктор объекта класса
+    // Конструктор объекта класса (создаёт массив из 10 стандартных значений)
     public MagicArray() {
-        array = new int[0];
+        array = new int[10];
     }
 
-    // 1. Добавление в массив элементов
-
-    // 1.1. Добавление одного элемента
+    // 1.1. Добавление в массив одного элемента
 
     void add (int value) {
 
-        if (cursor == 0 || cursor == array.length - 1) expandArray();
+        if (cursor == array.length - 1) expandArray();
         array[cursor] = value;
-    }
-
-    void add(int... numbers) {
-        // c numbers возможно обращаться точно также, как со ссылкой на массив int
-        // System.out.println("Приняли несколько целых чисел типа int, а именно: " + numbers.length);
-        // System.out.println("Есть индекс у каждого числа, как массива. Под индексом 0: " + numbers[0]);
-        for (int i = 0; i < numbers.length; i++) add(numbers[i]);
-
+        cursor++;
     }
 
     // 2. Вывод в консоль значений массива
 
     void printArray () {
         System.out.print("[");
-        for (int i = 0; i < array.length; i++) System.out.print(array[i] + ((i < array.length - 1) ? ", " : ""));
+        for (int i = 0; i < cursor; i++) System.out.print(array[i] + ((i < cursor - 1) ? ", " : ""));
         System.out.print("]\n");
     }
 
-    // Динамическое расширение массива на 1 элемент (если не передан аргумент)
-    void expandArray() {
-        int[] newArray = new int[cursor];
-        for (int i = 0; i < cursor; i++) newArray[i] = array[i];
-        array = newArray;
-        cursor++;
+    // 3. Бинарный поиск элемента в массиве с возвращением его индекса (если элемент найден),
+    // либо - 1 (если элемент не найден)
+
+    int binarySearch (int searchValue) {
+        int startIndex = 0;
+        int endIndex = cursor - 1;
+        int middleIndex;
+        while (startIndex <= endIndex) {
+            middleIndex = startIndex + (endIndex - startIndex) / 2;
+            if (array[middleIndex] == searchValue) return middleIndex;
+            if (array[middleIndex] > searchValue) endIndex = middleIndex - 1;
+            else startIndex = middleIndex + 1;
+        }
+        return -1;
     }
 
-    // Динамическое расширение массива на n элементов
-    void expandArray(int n) {
-        int[] newArray = new int[array.length + n];
-        for (int i = 0; i < cursor; i++) newArray[i] = array[i];
-        array = newArray;
-    }
+    // 4. Возвращение значения по индексу
 
-    // Д
-
-
-    // Текущее количество элементов в массиве
-    int size() {
-        return cursor;
-    }
-
-    // Возвращает значение по индексу
     int get(int index) {
-        if (index >= 0 && index > cursor) {
+        if (index >= 0 && index < cursor) {
             return array[index];
         }
         // Написать код, если индекс "некорректный"
@@ -68,28 +53,60 @@ public class MagicArray {
         //TODO: поправить обработку некорректного индекса
     }
 
-    int remove(int index) {
-        /*
-        1. Проверка индекса на валидность
-        2. Удалить значение по индексу
-        3. Передвинуть курсор (т.к. количество элементов уменьшилось)
-        4. Вернуть старое значение
-         */
+    // 5. Возвращение текущего количества элементов в массиве
 
-        return 0;
+    int size() {
+        return cursor;
+    }
+
+    //6. Удаление элемента по индексу index, возвращается значение удалённого элемента
+
+    int removeIndex(int index) {
+        if (index > 0 && index < cursor) {
+            int tmp = array[index];
+            for (int i = index; i < cursor; i++) {
+                array[i] = array[i + 1];
+            }
+            cursor--;
+            return tmp;
+        }
+        return Integer.MIN_VALUE;
+    }
+
+    // 7. Добавление в массив нескольких элементов
+
+        void add(int... numbers) {
+            for (int i = 0; i < numbers.length; i++) add(numbers[i]);
+        }
+
+    // 8. Конструктор, принимающий в себя обычный массив и создающий MagicArray с такими же значениями
+    public MagicArray(int[] ordinaryArray) {
+        if (ordinaryArray == null || ordinaryArray.length == 0) array = new int[10];
+        else {
+            array = ordinaryArray;
+            cursor = ordinaryArray.length;
+            expandArray();
+        }
+    }
+
+    // 9. Удаление элемента по значению, возвращается индекс удалённого элемента
+
+    int removeValue(int value) {
+        int index = binarySearch(value);
+        if (index == -1) return -1;
+        else {
+            removeIndex(index);
+            return index;
+        }
 
     }
 
+    // Динамическое расширение массива в два раза
+
+    void expandArray() {
+        int[] newArray = new int[array.length * 2];
+        for (int i = 0; i < cursor; i++) newArray[i] = array[i];
+        array = newArray;
+    }
+
 }
-
-
-/*
-1. Добавлять в массив элемент (не думая об индексах, размере массива)
-2. Динамическое изменение размера массива.
-3. Возвращать строковое представление массива (какие элементы там есть).
-4. Добавляем в массив сразу несколько значений.
-5. Текущее количество элементов в массиве.
-6. Возвращает значение по индексу
-7. Удаляет элемент по индексу (есть индекс - удалить этот элемент из массива). Вернуть старое значение
-
- */
--- src/homework_16/Task0.java
+++ src/homework_16/Task0.java
@@ -4,7 +4,7 @@ package homework_16;
 Task 0
 Повторить код, написанный на уроке
 
-В резиновом массиве должны быть реализованы методы:
+В MagicArray должны быть реализованы методы:
     1. Добавление элемента
     2. Вывести все значения в массиве в консоль
     3. Поиск элемента по значению indexOf - (вернуть индекс, если такое значение есть в массиве)
@@ -12,12 +12,11 @@ Task 0
     5. Текущее количество элементов в массиве
     6. Удаление элемента по индексу remove
     7. Добавление нескольких элементов (в качестве аргументов передает последовательность значений)
-    8. Конструктор, принимающий в себя обычный массив и создающий MagicArray с такими де значениями
+    8. Конструктор, принимающий в себя обычный массив и создающий MagicArray с такими же значениями
     9. Удаление элемента по значению
  */
 
 public class Task0 {
-
 
     public static void main(String[] args) {
 
@@ -28,6 +27,43 @@ public class Task0 {
         System.out.println("\n2. Добавляем в массив элемент со значением 10:");
         array.add(10);
         array.printArray();
+
+        System.out.println("\n3. Добавляем в массив элементы со значениям 20, 30, 40, 50:");
+        array.add(20, 30, 40, 50);
+        array.printArray();
+
+        System.out.println("\n4. Ищем индекс элемента со значением 40:");
+        System.out.println(array.binarySearch(40));
+
+        System.out.println("\n5. Пытаемся найти индекс элемента со значением 45:");
+        System.out.println(array.binarySearch(45));
+
+        System.out.println("\n6. Выводим значение элемента с индексом 1:");
+        System.out.println(array.get(1));
+
+        System.out.println("\n7. Выводим текущее количество элементов в массиве:");
+        System.out.println(array.size());
+
+        System.out.println("\n8. Удаляем из массива значение с индексом 3:");
+        System.out.println("Удалённое значение: " + array.removeIndex(3));
+        array.printArray();
+
+        System.out.println("\n9. Пытаемся удалить из массива отсутствующее значение с индексом 10:");
+        System.out.println("Удалённое значение: " + array.removeIndex(10));
+        array.printArray();
+
+        System.out.println("\n10. Создаём MagicArray из обычного массива [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]:");
+        int[] ordinaryArray = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        MagicArray array2 = new MagicArray(ordinaryArray);
+        array2.printArray();
+
+        System.out.println("\n11. Удаляем из нового массива значение 7:");
+        System.out.println("Индекс удалённого значения: " + array2.removeValue(7));
+        array2.printArray();
+
+        System.out.println("\n12. Пытаемся удалить из массива отсутствующее значение 10:");
+        System.out.println("Индекс удалённого значения: " + array2.removeValue(10));
+        array2.printArray();
 
     }
 
