233ea20804665cf522e023b8a4c6eace42e976b3
s-saratov <saratovs@yandex.ru>
2024-10-21 15:02:05 +0200

    lesson_28 complete

---
+++ src/lesson_28/EmployeeApp.java
@@ -0,0 +1,28 @@
+package lesson_28;
+
+import lists.MyArrayList;
+import lists.MyList;
+
+public class EmployeeApp {
+
+    public static void main(String[] args) {
+
+        MyList<Employee> employees = new MyArrayList<>();
+
+        Employee employee = new Employee("Пётр", "Петров", "21.10.2000");
+        Employee employee1 = new Employee("Иван", "Иванов", "05.12.1990");
+        Employee employee2 = new Employee("Иван", "Иванов", "15.01.2001");
+        Employee employee3 = new Employee("Иван", "Иванов", "15.01.2001");
+
+        System.out.println(employee == employee1);
+        System.out.println("employee == employee1:  " + (employee2 == employee3));
+
+        System.out.println("employee2.equals(employee):  " + (employee2.equals(employee)));
+
+
+    }
+
+}
+
+// TODO: узнать, зачем делается интерфейсная ссылка
+// TODO: попросить показать немного использование регулярных выражений
